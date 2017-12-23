c0d9d47ca23024392efdd346c6e05343f5d61e52
Frank-W-B <frank.burkholder@gmail.com>
2017-12-23 12:15:21 -0700

    Add working version of typist

--- typist.py
+++ typist.py
@@ -1,6 +1,7 @@
 import string
 import pickle
 from pathlib import Path
+import pdb
 
 
 class Typist(object):
@@ -20,34 +21,33 @@ class Typist(object):
         if self.filename.is_file():
             with open(self.filename, 'rb') as f:
                 User = pickle.load(f)
-                self.char_dict = User.char_dict
                 print(" {0}'s typing history has been loaded.".format(username))
+                return User.char_dict
         else:
             '''initialize number correct, tested for a new typist'''
             chars_to_type = (string.ascii_lowercase + string.digits 
                              +  string.punctuation) 
             char_lst = [char for char in chars_to_type] 
-            return {char: [0,0] for char in char_lst}  
             print(" New user created.") 
+            return {char: [0,0] for char in char_lst}  
 
-    def save(self, obj):
+    def save(self):
         '''pickles the Typist object'''
         self.char_dict['a'][1] += 1
         with open(self.filename, 'wb') as outfile:
-            pickle.dump(obj, outfile)
+            pickle.dump(self, outfile)
         
 
 
 if __name__ == '__main__':
     username = 'greyfalcon'
-    #pdb.set_trace()
     User = Typist(username)
-    char_dict = '\n'.join(['{0}, {1}'.format(k, v) for k, v in list(User.char_dict.items()[:10]))
+    char_dict = '\n'.join(['{0} {1}'.format(k, v) for k, v in User.char_dict.items()])
     print("After created") 
     print(char_dict)
-    User.save(User)
+    User.save()
     User2 = Typist(username)
-    char_dict2 = '\n'.join(['{0}, {1}'.format(k, v) for k, v in User2.char_dict.items()])
+    char_dict2 = '\n'.join(['{0} {1}'.format(k, v) for k, v in User2.char_dict.items()])
     print("\nAfter loaded")
     print(char_dict2)
 
